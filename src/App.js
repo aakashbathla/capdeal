12fd549b01421195cdfdf6d7a7ffab48045afa0f
aakashbathla <aakash.bathla7@gmail.com>
2020-06-13 14:55:35 +0530

    scss setup | initial Project Complete Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">Capdeal Application</div>;
+  return <div className="App flex">Capdeal Application</div>;
 }
 
 export default App;
