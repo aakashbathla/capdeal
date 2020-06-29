0c9215b99ad03c0fe7f21c47b8cbcb488cbad951
Deepak Maurya <deepak.maurya@thoughts2binary.com>
2020-06-29 20:51:29 +0530

    login page ui cleanups

--- src/components/molecules/Login/Login.js
+++ src/components/molecules/Login/Login.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+import Anchor from "components/atoms/Anchor/Anchor";
 import LoginLogo from "../../../assets/login-logo.png";
+import Logo from "../../../assets/logo.png";
+import bgImage from "assets/login-bg.png";
 import Button from "../../atoms/Button/";
 import { Redirect } from "react-router-dom";
 import "./Login.scss";
@@ -21,79 +24,96 @@ const Login = ({
     loginWithOtp(phoneNumber);
   };
   return (
-    <div className="login">
+    <div className="login" style={{backgroundImage: `url(`+bgImage+`)`}}>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-2 col-lg-2">
+              <div className="logo-area">
+                <Anchor to="/home">
+                  <img src={Logo} alt="enventer" />
+                </Anchor>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
       <div className="container">
-        <div className="row">
-          <div className="col-lg-12 center-box">
+        <div className="row align-items-center login_box">
+          <div className="col-lg-12">
             {!showOtpSuccess && (
-              <form onSubmit={handleSubmit} name="login_with_otp_form">
-                <div className="login_wrapper">
-                  <div className="login_wrapper_img text-center">
-                    <img src={LoginLogo} alt="login-logo" />
-                  </div>
-                  <div className="login_wrapper_content text-center">
-                    <div className="login_wrapper_content_header">
-                      Login with your Mobile Number
+              <form onSubmit={handleSubmit} name="login_with_otp_form" className="d-flex justify-content-center">
+                <div className="login_box_border col-xs-10 col-md-6 col-lg-5">
+                  <div className="login_wrapper">
+                    <div className="login_wrapper_img text-center">
+                      <img src={LoginLogo} alt="login-logo" />
                     </div>
-                    <div className="login_wrapper_content_subheader">
-                      Your information is safe with us
+                    <div className="login_wrapper_content text-center">
+                      <div className="login_wrapper_content_header">
+                        Login with your Mobile Number
+                      </div>
+                      <div className="login_wrapper_content_subheader mt-1 mb-3">
+                        Your information is safe with us
+                      </div>
                     </div>
-                  </div>
-                  <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text" id="basic-addon1">
-                        +91
+                    <div className="input-group mb-3">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text" id="basic-addon1">
+                          +91
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter Your Number"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        onChange={(e) => setPhoneNumber(`+91${e.target.value}`)}
+                      />
+                    </div>
+                    <div className="login_cta col-xs-12 d-flex justify-content-between">
+                      <Button className="login_btn btn btn-lg btn-outline-primary" type="submit">
+                        Login With Otp
+                      </Button>
+                      <Button className="btn btn-lg btn-outline-primary">Login With Password</Button>
+                    </div>
+                    <div className="login_wrapper_already_transacted mt-3">
+                      Already Transacted with Capdeal?
+                      <span className="login_wrapper_already_transacted_link">
+                        Click here to Login
                       </span>
                     </div>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Enter Your Number"
-                      aria-label="Username"
-                      aria-describedby="basic-addon1"
-                      onChange={(e) => setPhoneNumber(`+91${e.target.value}`)}
-                    />
-                  </div>
-                  <div className="login_cta">
-                    <Button className="col-6" type="submit">
-                      Login With Otp
-                    </Button>
-                    <Button className="col-6">Login With Password</Button>
-                  </div>
-                  <div className="login_wrapper_already_transacted">
-                    Already Transacted with Capdeal?
-                    <span className="login_wrapper_already_transacted_link">
-                      Click here to Login
-                    </span>
                   </div>
                 </div>
               </form>
             )}
             {showOtpSuccess && (
-              <form onSubmit={submitOtpFunc} name="submit_otp_form">
-                <div className="login_wrapper">
-                  <div className="login_wrapper_img text-center">
-                    <img src={LoginLogo} alt="login-logo" />
-                  </div>
-                  <div className="login_wrapper_content text-center">
-                    <div className="login_wrapper_content_header">
-                      Please Enter OTP
+              <form onSubmit={submitOtpFunc} name="submit_otp_form" className="d-flex justify-content-center">
+                <div className="login_box_border col-xs-10 col-md-6 col-lg-5">
+                  <div className="login_wrapper">
+                    <div className="login_wrapper_img text-center">
+                      <img src={LoginLogo} alt="login-logo" />
                     </div>
-                  </div>
-                  <div className="input-group mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Enter OTP"
-                      aria-label="Username"
-                      aria-describedby="basic-addon1"
-                      onChange={(e) => setOtp(`${e.target.value}`)}
-                    />
-                  </div>
-                  <div className="login_cta text-center">
-                    <Button className="col-6 text-center" type="submit">
-                      Submit Otp
-                    </Button>
+                    <div className="login_wrapper_content text-center">
+                      <div className="login_wrapper_content_header mb-4">
+                        Please Enter OTP
+                      </div>
+                    </div>
+                    <div className="input-group mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter OTP"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        onChange={(e) => setOtp(`${e.target.value}`)}
+                      />
+                    </div>
+                    <div className="login_cta text-center">
+                      <Button className="col-6 text-center btn btn-lg btn-outline-primary" type="submit">
+                        Submit Otp
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </form>
