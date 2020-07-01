e62e7c3fdcdb926ba6d2de3bd98772179221c3a6
aakashbathla <aakash.bathla7@gmail.com>
2020-07-01 22:07:11 +0530

    linting issue

--- src/constants/apis/services.js
+++ src/constants/apis/services.js
@@ -2,7 +2,7 @@ const api_url = "http://api.capdeal.in";
 const apis = {
   loginwithotp: `${api_url}/user/generate-otp/`,
   submitotp: `${api_url}/user/login-with-otp/`,
-  socialLogin: `${api_url}/social-sign-up/`,
+  socialLogin: `${api_url}/social-sign-up`,
 };
 
 export default apis;
