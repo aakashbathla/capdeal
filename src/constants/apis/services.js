c205e40c26bed19e28ac5b4ea36145fc6ede5e71
Aman Kumar <amankumar0511@gmail.com>
2020-07-11 22:22:22 +0530

    ssl url added

--- src/constants/apis/services.js
+++ src/constants/apis/services.js
@@ -1,4 +1,4 @@
-const api_url = "http://api.capdeal.in";
+const api_url = "https://api.capdeal.in";
 const apis = {
   loginwithotp: `${api_url}/user/generate-otp/`,
   submitotp: `${api_url}/user/login-with-otp/`,
