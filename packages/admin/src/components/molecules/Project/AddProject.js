1f961fb43bd55ae895e6d536f84d99ddc1080f4d
Deepak Maurya <deepak.maurya@thoughts2binary.com>
2020-09-08 16:07:01 +0530

    admin:project add and edit form ui

--- packages/admin/src/components/molecules/Project/AddProject.js
+++ packages/admin/src/components/molecules/Project/AddProject.js
@@ -2,7 +2,7 @@
 import Form from "@rjsf/core";
 import React from "react";
 import { useHistory } from "react-router";
-import { schema, uiSchema } from "./AddProjectSchema";
+import { schema, uiSchema, fields } from "./AddProjectSchema";
 import { addData } from "../../../utils/Utils";
 import apis from "../../../constants/apis/services";
 const AddProject = () => {
@@ -13,13 +13,17 @@ const AddProject = () => {
     });
   };
   return (
-    <div className="col-lg-6 col-md-6 col-sm-12">
-      <span className="back-btn" onClick={() => history.goBack()}>
-        <i className="zmdi zmdi-arrow-left"></i>
-      </span>
+    <div>
+      <div className="form-header">
+        <span className="back-btn" onClick={() => history.goBack()}>
+          <i className="zmdi zmdi-arrow-left"></i>
+        </span>
+        <span className="form-header__title">Add Project</span>
+      </div>
       <Form
         schema={schema}
         uiSchema={uiSchema}
+        fields={fields}
         onSubmit={({ formData }, e) => {
           console.log(formData);
           e.preventDefault();
