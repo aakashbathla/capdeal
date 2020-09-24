fc1c33a315c95f99ae39a215e8eb393f3d00caf4
aakashbathla <aakash.bathla7@gmail.com>
2020-09-24 12:45:17 +0530

    project detail dynamic images

--- packages/app/src/components/molecules/ProjectDetailSlider/ProjectDetailSlider.js
+++ packages/app/src/components/molecules/ProjectDetailSlider/ProjectDetailSlider.js
@@ -1,55 +1,62 @@
 import React, { useState } from "react";
-import ImageGallery from 'react-image-gallery';
+import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/scss/image-gallery.scss";
 import ProjectDetail1 from "assets/project-detail11.png";
 import ProjectDetail2 from "assets/project-detail1.png";
 import SampleVideo from "assets/sample-video.mp4";
 import "./ProjectDetailSlider.scss";
 
-const images = [
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-  {
-    original: ProjectDetail2,
-    thumbnail: ProjectDetail1,
-  },
-];
+// const images = [
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+//   {
+//     original: ProjectDetail2,
+//     thumbnail: ProjectDetail1,
+//   },
+// ];
 
 const ProjectDetailSlider = ({ data }) => {
+  console.log(data);
+  const images =
+    data &&
+    data.media &&
+    data.media.map((val, key) => {
+      return { original: val.media_file, thumbnail: val.media_file };
+    });
   const [status, setStatus] = useState(false);
   const settings = {
     infinite: true,
@@ -57,25 +64,36 @@ const ProjectDetailSlider = ({ data }) => {
     showPlayButton: false,
     autoPlay: false,
     showNav: false,
-    thumbnailPosition: 'right',
+    thumbnailPosition: "right",
   };
 
   return (
     <div className="row">
       <div className="col-md-12 col-12">
-        <ImageGallery items={images}  {...settings} />
-        { status && 
+        <ImageGallery items={images} {...settings} />
+        {status && (
           <div className="video-wrap">
             <video width="100%" height="470" controls autoplay>
-              <source src={SampleVideo} type="video/mp4" />
+              <source
+                src={data && data.video && data.video.media_file}
+                type="video/mp4"
+              />
               Your browser does not support the video tag.
             </video>
-            <span title="Close Video" className="close-video" onClick={() => setStatus(!status)}><i className="zmdi zmdi-close"></i></span>
+            <span
+              title="Close Video"
+              className="close-video"
+              onClick={() => setStatus(!status)}
+            >
+              <i className="zmdi zmdi-close"></i>
+            </span>
           </div>
-        }
+        )}
         <div className="video-thumbnail" onClick={() => setStatus(!status)}>
           <img width="92" src={ProjectDetail1} alt="video" />
-          <span className="play-btn"><i className="zmdi zmdi-play-circle-outline"></i></span>
+          <span className="play-btn">
+            <i className="zmdi zmdi-play-circle-outline"></i>
+          </span>
         </div>
       </div>
     </div>
