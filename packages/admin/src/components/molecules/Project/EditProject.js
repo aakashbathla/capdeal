1356206576df2903ed94e2e3e7283900a7101e3a
aakashbathla <aakash.bathla7@gmail.com>
2020-10-11 23:45:31 +0530

    edit project issue fixed

--- packages/admin/src/components/molecules/Project/EditProject.js
+++ packages/admin/src/components/molecules/Project/EditProject.js
@@ -4,28 +4,43 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import { schema, uiSchema, fields } from "./EditProjectSchema";
 import apis from "../../../constants/apis/services";
-import { fetchData, updateData, errorGenerator } from "../../../utils/Utils";
+import {
+  fetchData,
+  updateData,
+  errorGenerator,
+  transformErrors,
+} from "../../../utils/Utils";
 import "./Project.scss";
 
 const EditProject = (props) => {
   const history = useHistory();
+  const [error, setError] = useState(null);
   const [updateFormDataValue, setUpdateFormDataValue] = useState(null);
   const updateFormData = (data) => {
-    data.rera_no = parseInt(data.rera_no);
-    data.video = parseInt(data.video.id);
-    data.broucher = parseInt(data.broucher.id);
-    data.mediaFile = data.media
-      .map((val) => {
-        return val.id;
-      })
-      .toString();
+    if (data.rera_no) {
+      data.rera_no = parseInt(data.rera_no);
+    }
+    if (data.video && data.video.id) {
+      data.video = parseInt(data.video.id);
+    }
+    if (data.broucher && data.broucher.id) {
+      data.broucher = parseInt(data.broucher.id);
+    }
+    if (data.media) {
+      data.mediaFile = data.media;
+    }
+    if (data && data.price_range) {
+      data.price_range_min = parseFloat(data.price_range[0]);
+      data.price_range_max = parseFloat(data.price_range[1]);
+    }
     if (data.amenities) {
       for (var i = 0; i < data.amenities.length; i++) {
-        data.amenities[i].mediaFile = data.amenities[i].media
-          .map((val) => {
-            return val.id;
-          })
-          .toString();
+        data.amenities[i].mediaFile = data.amenities[i].media;
+      }
+    }
+    if (data.features) {
+      for (var i = 0; i < data.features.length; i++) {
+        data.features[i].mediaFile = data.features[i].media || undefined;
       }
     }
     setUpdateFormDataValue(data);
@@ -36,7 +51,7 @@ const EditProject = (props) => {
     });
   };
   const errorHandler = (err) => {
-    console.log(errorGenerator(err));
+    setError(errorGenerator(err));
   };
   useEffect(() => {
     if (props && props.match && props.match.params && props.match.params.id) {
@@ -56,12 +71,21 @@ const EditProject = (props) => {
         </span>
         <span className="form-header__title">Edit Project</span>
       </div>
+      {error && (
+        <div className="error">
+          {error.map((val, key) => (
+            <div>{val}</div>
+          ))}
+        </div>
+      )}
       {updateFormDataValue && (
         <Form
           schema={schema}
           uiSchema={uiSchema}
           fields={fields}
           formData={updateFormDataValue}
+          transformErrors={transformErrors}
+          noHtml5Validate={true}
           onSubmit={({ formData }, e) => {
             e.preventDefault();
             if (formData && formData.mediaFile) {
@@ -75,7 +99,21 @@ const EditProject = (props) => {
                   : [];
               });
               for (var i = 0; i < formData.amenities.length; i++) {
-                delete formData.amenities[i].mediaFile;
+                if (formData.amenities[i] && formData.amenities[i].mediaFile) {
+                  delete formData.amenities[i].mediaFile;
+                  delete formData.amenities[i].id;
+                }
+              }
+            }
+            if (formData && formData.features) {
+              formData.features.map((val, key) => {
+                formData.features[key].media = parseInt(val.mediaFile);
+              });
+
+              for (var i = 0; i < formData.features.length; i++) {
+                if (formData.features[i] && formData.features[i].mediaFile) {
+                  delete formData.features[i].mediaFile;
+                }
               }
             }
             updateData(
--- packages/admin/src/components/molecules/Project/EditProjectSchema.js
+++ packages/admin/src/components/molecules/Project/EditProjectSchema.js
@@ -6,12 +6,7 @@ import apis from "../../../constants/apis/services";
 export const schema = {
   type: "object",
   title: "Edit Project",
-  required: [
-    "developer",
-    "name",
-    "rera_no",
-    "address_line1"
-  ],
+  required: ["developer", "name", "rera_no", "address_line1"],
   properties: {
     developer: {
       type: "integer",
@@ -25,7 +20,7 @@ export const schema = {
     description: {
       type: "string",
       title: "Description",
-      minLength: 3
+      minLength: 3,
     },
     rera_no: {
       type: "integer",
@@ -50,6 +45,18 @@ export const schema = {
     zipcode: {
       type: "string",
       title: "Zipcode",
+    },
+    media_url: {
+      type: "string",
+      title: "Video Url",
+    },
+    min_price: {
+      type: "number",
+      title: "Price Range Min Value",
+    },
+    max_price: {
+      type: "number",
+      title: "Price Range Max Value",
     },
     home_page: {
       type: "boolean",
@@ -78,10 +85,6 @@ export const schema = {
     property_type: {
       type: "string",
       title: "Property Type",
-    },
-    mediaFile: {
-      type: "string",
-      title: "Media",
     },
     floor_plans: {
       type: "array",
@@ -129,42 +132,86 @@ export const schema = {
             title: "Media",
           },
         },
+        required: ["name", "category", "mediaFile"],
+      },
+    },
+    mediaFile: {
+      type: "string",
+      title: "Media",
+    },
+    features: {
+      type: "array",
+      title: "Features",
+      items: {
+        type: "object",
+        properties: {
+          description: {
+            type: "string",
+            title: "Description",
+          },
+          mediaFile: {
+            type: "string",
+            title: "Media",
+          },
+        },
+        required: ["description", "mediaFile"],
       },
     },
   },
+  required: [
+    "developer",
+    "amenities",
+    "name",
+    "status",
+    "property_type",
+    "min_price",
+    "max_price",
+    "features",
+  ],
 };
 export const fields = {
   layout_grid: LayoutGridField,
-}
+};
 
 export const uiSchema = {
-  'ui:field': 'layout_grid',
-  'ui:layout_grid': { 'ui:row': [
-    { 'ui:col': { md: 12, children: [
-      { 'ui:row': [
-        { 'ui:col': { md: 6, children: ['developer'] } },
-        { 'ui:col': { md: 6, children: ['name'] } },
-        { 'ui:col': { md: 6, children: ['rera_no'] } },
-        { 'ui:col': { md: 6, children: ['address_line1'] } },
-        { 'ui:col': { md: 6, children: ['address_line2'] } },
-        { 'ui:col': { md: 6, children: ['city'] } },
-        { 'ui:col': { md: 6, children: ['state'] } },
-        { 'ui:col': { md: 6, children: ['zipcode'] } },
-        { 'ui:col': { md: 6, children: ['status'] } },
-        { 'ui:col': { md: 6, children: ['property_type'] } },
-        { 'ui:col': { md: 12, children: ['description'] } },
-        { 'ui:col': { md: 6, children: ['video'] } },
-        { 'ui:col': { md: 6, children: ['broucher'] } },
-        { 'ui:col': { md: 6, children: ['mediaFile'] } },
-        { 'ui:col': { md: 3, children: ['home_page'] } },
-        { 'ui:col': { md: 3, children: ['is_feature'] } },
-        { 'ui:col': { md: 12, children: ['floor_plans'] } },
-        { 'ui:col': { md: 12, children: ['amenities'] } },
-      ] },
-      ]
-    }
-  }
-  ]},
+  "ui:field": "layout_grid",
+  "ui:layout_grid": {
+    "ui:row": [
+      {
+        "ui:col": {
+          md: 12,
+          children: [
+            {
+              "ui:row": [
+                { "ui:col": { md: 6, children: ["developer"] } },
+                { "ui:col": { md: 6, children: ["name"] } },
+                { "ui:col": { md: 6, children: ["rera_no"] } },
+                { "ui:col": { md: 6, children: ["address_line1"] } },
+                { "ui:col": { md: 6, children: ["address_line2"] } },
+                { "ui:col": { md: 6, children: ["city"] } },
+                { "ui:col": { md: 6, children: ["state"] } },
+                { "ui:col": { md: 6, children: ["zipcode"] } },
+                { "ui:col": { md: 12, children: ["media_url"] } },
+                { "ui:col": { md: 3, children: ["price_range_min"] } },
+                { "ui:col": { md: 3, children: ["price_range_max"] } },
+                { "ui:col": { md: 6, children: ["status"] } },
+                { "ui:col": { md: 6, children: ["property_type"] } },
+                { "ui:col": { md: 12, children: ["description"] } },
+                { "ui:col": { md: 6, children: ["video"] } },
+                { "ui:col": { md: 6, children: ["broucher"] } },
+                { "ui:col": { md: 6, children: ["mediaFile"] } },
+                { "ui:col": { md: 3, children: ["home_page"] } },
+                { "ui:col": { md: 3, children: ["is_feature"] } },
+                { "ui:col": { md: 12, children: ["floor_plans"] } },
+                { "ui:col": { md: 12, children: ["amenities"] } },
+                { "ui:col": { md: 12, children: ["features"] } },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  },
   description: {
     "ui:widget": "textarea",
   },
@@ -172,7 +219,7 @@ export const uiSchema = {
     items: {
       mediaFile: {
         "ui:widget": MultipleMediaPreviewWidget,
-        "classNames": "upload-media"
+        classNames: "upload-media",
       },
       category: {
         "ui:widget": getList,
@@ -182,19 +229,44 @@ export const uiSchema = {
       },
     },
   },
+  floor_plans: {
+    items: {
+      title: {
+        "ui:widget": getList,
+        "ui:options": {
+          url: apis.floorPlanChoice,
+          type: "normal",
+        },
+      },
+    },
+  },
+  features: {
+    items: {
+      mediaFile: {
+        "ui:widget": MultipleMediaPreviewWidget,
+        classNames: "upload-media",
+        "ui:options": {
+          number: 1,
+        },
+      },
+      description: {
+        "ui:widget": "textarea",
+      },
+    },
+  },
   broucher: {
     "ui:widget": MediaApiWidget,
     "ui:options": { accept: ".pdf" },
-    "classNames": "file-upload"
+    classNames: "file-upload",
   },
   mediaFile: {
     "ui:widget": MultipleMediaPreviewWidget,
-    "classNames": "upload-media"
+    classNames: "upload-media",
   },
   video: {
     "ui:widget": MediaApiWidget,
-    "ui:options": { accept: "video/*" },
-    "classNames": "file-upload"
+    "ui:options": { accept: "video/*", type: "video" },
+    classNames: "file-upload",
   },
   developer: {
     "ui:widget": getList,
